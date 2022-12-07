b02020b2b732415da96cfd3442407cdaeab5e056
MscCagri <99085961+MscCagri@users.noreply.github.com>
2022-12-07 22:44:19 +0300

    Error folder added and UserController update

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,14 +1,17 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -19,13 +22,27 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/api/1.0/users")
-    @ResponseStatus(HttpStatus.CREATED)
-    public GenericResponse createUser(@RequestBody User user){
+    public ResponseEntity<?> createUser(@RequestBody User user){
+        String username = user.getUsername();
+        String displayName = user.getDisplayName();
 
-        log.info(user.toString());
+        ApiError error = new ApiError(400 ,"Validation Error","/api/1.0/users");
+        Map<String, String> validationErrors = new HashMap<>();
+
+        if( username == null || username.isEmpty() ){
+            validationErrors.put("username", "username cannot be null");
+        }
+
+        if( displayName == null || displayName.isEmpty() ){
+            validationErrors.put("displayName", " cannot be null");
+        }
+        if(validationErrors.size()>0){
+            error.setValidationErrors(validationErrors);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
+
         userService.save(user);
-
-        return new GenericResponse("User Created");
+        return ResponseEntity.ok(new GenericResponse("User Created"));
     }
 
 
